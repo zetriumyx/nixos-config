220ad13b85f3dc540c7003eb66c1de95e915f717
Zetrium Zurain <zetrium@outlook.com>
2026-03-09 14:30:20 +0800

    removed unused locales

--- desktop/modules/localization/default.nix
+++ desktop/modules/localization/default.nix
@@ -6,6 +6,6 @@
     ./fonts.nix
   ];
   time.timeZone = "Asia/Shanghai";
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocales = [ "zh_CN.UTF-8/UTF-8" ];
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.extraLocales = [ "en_US.UTF-8/UTF-8" ];
 }
