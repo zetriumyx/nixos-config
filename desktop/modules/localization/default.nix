696eb2889085e30ab87a6331ced758f4ec382455
Zetrium Zurain <zetrium@outlook.com>
2026-03-09 12:31:06 +0800

    fixed locales

--- desktop/modules/localization/default.nix
+++ desktop/modules/localization/default.nix
@@ -7,5 +7,5 @@
   ];
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = ["en_US.UTF-8" "zh_CN.UTF-8"];
+  i18n.extraLocales = [ "zh_CN.UTF-8" ];
 }
