7e3b9c63d2a864af7cd4158fd6e5021a625493cd
Zetrium Zurain <zetrium@outlook.com>
2026-03-11 21:06:41 +0800

    fixed-bluetooth

--- desktop/modules/hardware/default.nix
+++ desktop/modules/hardware/default.nix
@@ -6,4 +6,18 @@
     ./audio.nix
   ];
   services.printing.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
 }
